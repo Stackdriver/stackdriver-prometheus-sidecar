8c391b4f35454458b0c6c0e00b22339eac7aac3b
Fabian Reinartz <freinartz@google.com>
2018-07-04 14:24:33 -0400

    Remove sample batching restriction
    Signed-off-by: Fabian Reinartz <freinartz@google.com>

--- cmd/stackdriver-prometheus-sidecar/main.go
+++ cmd/stackdriver-prometheus-sidecar/main.go
@@ -131,7 +131,8 @@ func main() {
 	// types. Currently Stackdriver fails the entire request if you attempt
 	// to write to the different metric type, which we do fairly often at
 	// this point, so lots of writes fail, and most writes fail.
-	config.DefaultQueueConfig.MaxSamplesPerSend = 1
+	config.DefaultQueueConfig.MaxSamplesPerSend = 30000
+	config.DefaultQueueConfig.MaxShards = 1000
 	var (
 		queueManager = stackdriver.NewQueueManager(
 			log.With(logger, "component", "queue_manager"),
