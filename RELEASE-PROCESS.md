1ed1d0b7b40e127647e2ba523688535532bef700
Javier Kohen <jkohen@gmail.com>
2018-10-12 11:47:36 -0400

    Documented release process (#50)
    * Update release.sh
    
    * Create RELEASE-PROCESS.md

---
+++ RELEASE-PROCESS.md
@@ -0,0 +1,3 @@
+# Release Process
+
+Make sure the [end-to-end test](https://github.com/Stackdriver/stackdriver-prometheus-e2e) passes, then run `./release.sh {VERSION}`.
--- release.sh
+++ release.sh
@@ -24,18 +24,6 @@ echo "${version}" > VERSION
 git checkout -b "release-${version}"
 
 # 3. Run `DOCKER_IMAGE_NAME={public_docker_image} make push`.
-DOCKER_IMAGE_NAME="gcr.io/stackdriver-prometheus/stackdriver-prometheus-sidecar" make push
-
-##########################
-# Deploy Prometheus server
-##########################
-
-# 1. Start from the configuration for the previous release and merge any changes from the master branch.
-# NOOP
-
-# 2. Update the version tag in the Docker image to the version you released.
-/bin/sh -c "${SED_I} -E 's/(image: ).*/\1gcr.io\/stackdriver-prometheus\/stackdriver-prometheus-sidecar:release-${version}/g' documentation/examples/prometheus-service.yml"
-
-# 3. Apply it to your cluster to verify it works.
-kubectl apply -f documentation/examples/prometheus-service.yml
-kubectl apply -f documentation/examples/rbac-setup.yml --as=admin --as-group=system:masters
+# TODO(jkohen): This is the public repo. It should replace the line below when we go public. 
+#DOCKER_IMAGE_NAME="gcr.io/stackdriver-prometheus/stackdriver-prometheus-sidecar" make push
+DOCKER_IMAGE_NAME="gcr.io/prometheus-to-sd/stackdriver-prometheus-sidecar" make push
