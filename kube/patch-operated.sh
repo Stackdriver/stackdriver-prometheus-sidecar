f2e638a59c6b18eda1c3f2986257ae1e8d2f12f7
Javier Kohen <jkohen@google.com>
2018-12-28 11:01:45 -0500

    Update patch-operated.sh

--- kube/patch-operated.sh
+++ kube/patch-operated.sh
@@ -8,6 +8,9 @@ if [  $# -le 1 ]; then
   exit 1
 fi
 
+# Override to use a different Docker image version for the sidecar.
+export SIDECAR_IMAGE_TAG=${SIDECAR_IMAGE_TAG:-'stable'}
+
 kubectl -n "${KUBE_NAMESPACE}" patch prometheus "$1" --type merge --patch "
 spec:
   containers:
