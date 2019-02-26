36dc15b358ec18abde8d2fd15ac933e7d3650341
Nina-Marie Amadeo <namadeo@google.com>
2019-02-26 14:41:08 -0500

    Adds resolver and resCleanup to clientFactory in main.go

--- cmd/stackdriver-prometheus-sidecar/main.go
+++ cmd/stackdriver-prometheus-sidecar/main.go
@@ -184,7 +184,6 @@ func main() {
 		filters            []string
 		metricRenames      map[string]string
 		staticMetadata     []scrape.MetricMetadata
-		ipOverride         bool
 
 		logLevel promlog.AllowedLevel
 	}{}
@@ -203,9 +202,6 @@ func main() {
 
 	a.Flag("stackdriver.api-address", "Address of the Stackdriver Monitoring API.").
 		Default("https://monitoring.googleapis.com:443/").URLVar(&cfg.stackdriverAddress)
-
-	a.Flag("stackdriver.api-ip-override", "List of IP addresses. If not empty, stackdriver.api-address will resolve to one of these addresses.").
-		BoolVar(&cfg.ipOverride)
 
 	a.Flag("stackdriver.kubernetes.location", "Value of the 'location' label in the Kubernetes Stackdriver MonitoredResources.").
 		StringVar(&cfg.kubernetesLabels.location)
