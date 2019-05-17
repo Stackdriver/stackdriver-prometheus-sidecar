14ff19f5046f15a4249190c4bcdaaafed1d32616
Yen-Cheng Chou <ycchou@google.com>
2019-05-17 12:20:24 -0400

    Remove VERSION file to avoid confusion.
    VERSION file is not updated on master branch, and local VERSION file can
    be confusing. point user to release page on GitHub as source of truth.

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ versioned_release_tag="v${version}"
 
 if [[ -z "${version}" ]]; then
     echo "Please provide a version: ./release.sh {VERSION}"
-    echo "The current version is: $(cat VERSION)"
+    echo "Look up latest version on https://github.com/Stackdriver/stackdriver-prometheus-sidecar/tags."
     exit 1
 fi
 
@@ -19,23 +19,16 @@ fi
 # Build and release docker image
 ################################
 
-# 1. Update file `VERSION` with the numeric version, e.g. `0.3.1`.
-echo "${version}" > VERSION
-
-# 2. Create a git branch for the version, e.g. `release-0.3.1`.
+# 1. Create a git branch for the version, e.g. `release-0.3.1`.
 git checkout -b "${versioned_release_branch}"
 
-# 3. Commit the version update.
-git add VERSION
-git commit -m "Update version to ${version}."
-
-# 4. Run `DOCKER_IMAGE_NAME={public_docker_image} make push`.
+# 2. Run `DOCKER_IMAGE_NAME={public_docker_image} make push`.
 DOCKER_IMAGE_NAME="gcr.io/stackdriver-prometheus/stackdriver-prometheus-sidecar" make push
 
 ################################
 # Push branch and tag to GitHub
 ################################
-# 1. Tag the commit to a specific version, e.g. `v0.3.1`. 
+# 1. Tag the commit to a specific version, e.g. `v0.3.1`.
 git tag "${versioned_release_tag}"
 
 # 2. Push release branch to GitHub.
