41ca65e5efc1577ac88a7c54b61e96770377f72f
Yen-Cheng Chou <ycchou@google.com>
2019-09-27 16:10:16 -0400

    Add unittest for context timeout.

--- stackdriver/client_test.go
+++ stackdriver/client_test.go
@@ -43,6 +43,33 @@ func newLocalListener() net.Listener {
 	return l
 }
 
+func TestStoreContextExceed(t *testing.T) {
+	listener := newLocalListener()
+	grpcServer := grpc.NewServer()
+	monitoring.RegisterMetricServiceServer(grpcServer, &metricServiceServer{nil})
+	go grpcServer.Serve(listener)
+	defer grpcServer.Stop()
+
+	serverURL, err := url.Parse("https://" + listener.Addr().String() + "?auth=false")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c := NewClient(&ClientConfig{
+		URL:     serverURL,
+		Timeout: 0, // Immeditate Timeout.
+	})
+
+	err = c.Store(&monitoring.CreateTimeSeriesRequest{
+		TimeSeries: []*monitoring.TimeSeries{
+			&monitoring.TimeSeries{},
+		},
+	})
+	if _, recoverable := err.(recoverableError); !recoverable {
+		t.Errorf("expected recoverableError in error %v", err)
+	}
+}
+
 func TestStoreErrorHandling(t *testing.T) {
 	tests := []struct {
 		status      *status.Status
