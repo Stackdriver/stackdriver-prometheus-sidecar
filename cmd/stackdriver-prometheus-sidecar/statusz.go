f73611166f923bbd649ff818c8bfbe297f38e9a3
fifiug <lynnasiimwe@google.com>
2020-02-02 11:55:18 +0000

    Updating statik

--- cmd/stackdriver-prometheus-sidecar/statusz.go
+++ cmd/stackdriver-prometheus-sidecar/statusz.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate statik -src=./  -include=*.html
+//go:generate statik -f -src=./  -include=*.html
 package main
 
 import (
